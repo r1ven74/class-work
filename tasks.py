d97c7922a8f50256a0f70e585a97c9c7a2c19033
r1ven74 <mancerovplaton@gmail.com>
2024-10-01 11:21:46 +0700

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,8 +1,8 @@
-numbers = list(map(int, input("введите сторону квадрата").split()))
+numbers = list(map(int, input("введите сторону квадрата").split())) # нахождение площади квадрата
 print(numbers[0]**2)
 
-numbers = list(map(int, input("введите стороны прямоугольника через пробел").split()))
+numbers = list(map(int, input("введите стороны прямоугольника через пробел").split())) #нахождение периметра прямоугольника
 print(numbers[0]*2+numbers[1]*2)
 
-numbers = list(map(int, input("введите радиус круга").split()))
+numbers = list(map(int, input("введите радиус круга").split())) #нахождения площади круга
 print(numbers[0]*2 *3.14)
